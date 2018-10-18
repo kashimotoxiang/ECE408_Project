5dbb78e0695b0f6292a8cdb96e6fda82063d121a
yuxiangli <kashimotoxiang@gmail.com>
2018-10-18 18:44:27 -0500

    master

---
+++ ece408_src/new-forward.h
@@ -0,0 +1,43 @@
+
+#ifndef MXNET_OPERATOR_NEW_FORWARD_H_
+#define MXNET_OPERATOR_NEW_FORWARD_H_
+
+#include <mxnet/base.h>
+
+namespace mxnet
+{
+namespace op
+{
+
+
+template <typename cpu, typename DType>
+void forward(mshadow::Tensor<cpu, 4, DType> &y, const mshadow::Tensor<cpu, 4, DType> &x, const mshadow::Tensor<cpu, 4, DType> &k)
+{
+    /*
+    Modify this function to implement the forward pass described in Chapter 16.
+    The code in 16 is for a single image.
+    We have added an additional dimension to the tensors to support an entire mini-batch
+    The goal here is to be correct, not fast (this is the CPU implementation.)
+    */
+
+    const int B = x.shape_[0];
+    // const int M = y.shape_[1];
+    // const int C = x.shape_[1];
+    // const int H = x.shape_[2];
+    // const int W = x.shape_[3];
+    // const int K = w.shape_[3];
+
+    for (int b = 0; b < B; ++b) {
+
+        CHECK_EQ(0,1) << "Remove this line and replace it with your implementation.";
+
+        /* ... a bunch of nested loops later...
+            y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
+        */
+    }
+
+}
+}
+}
+
+#endif
