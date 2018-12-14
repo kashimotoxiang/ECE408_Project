583c3ef8a53ef349c0e9ac3a7854ab15d1d7e9cc
Francis Ng <francisng0201@users.noreply.github.com>
2018-12-14 06:11:06 -0600

    0.95 with not much change

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -1,26 +1,21 @@
 #ifndef MXNET_OPERATOR_NEW_FORWARD_CUH_
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
-// Sweeping various parameters to find best values (block sizes, amount of
-// thread coarsening)
-#define TILE_WIDTH 16
-#define y4d(i3, i2, i1, i0)                                                    \
-    y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
 
-#define x4d(i3, i2, i1, i0)                                                    \
-    x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0)                                                    \
-    k_shared[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+#define x4d(i2, i1, i0) x[b + i2 * H * W + i1 * W + i0]
+#define y4d(i2, i1, i0) y[i2 * M * H_out * W_out + i1 * W_unroll + i0]
 
 #include <mxnet/base.h>
+#define TILE_WIDTH_LARGE 24
+#define TILE_WIDTH_SMALL 16
+#define K 7
+#define K_minus_one 6
+#define K_squre 49
 
 namespace mxnet {
 namespace op {
 
-__constant__ float k_shared[15000];
-
-__global__ void forward_kernel(float *y, const float *x, const float *k,
-                               const int B, const int M, const int C,
-                               const int H, const int W, const int K) {
+__global__ void forward_kernel_small(float* __restrict__ y, const float* __restrict__ x, const float* __restrict__ k,
+                            int M, int C, int H, int W, int H_unroll, int W_unroll) {
 
     /*
     Modify this function to implement the forward pass described in
@@ -29,107 +24,136 @@ __global__ void forward_kernel(float *y, const float *x, const float *k,
     have some nice #defs for you below to simplify indexing. Feel free to
     use them, or create your own.
     */
-    int X_tile_width = TILE_WIDTH + K - 1;
-    extern __shared__ float shmem[];
-    float *shared_x = &shmem[0];
+    __shared__ float shared_k[TILE_WIDTH_SMALL][TILE_WIDTH_SMALL];
+    __shared__ float shared_x[TILE_WIDTH_SMALL][TILE_WIDTH_SMALL];
 
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    const int tile_iw = ceil(W_out / (TILE_WIDTH * 1.0));
+    int b = blockIdx.z * C * H * W;
 
-    // An example use of these macros:
-    // float a = y4d(0,0,0,0)
-    // y4d(0,0,0,0) = a
+    const int H_out = H - K_minus_one;
+    const int W_out = W - K_minus_one;
 
-    int n = blockIdx.x;
-    int m = blockIdx.y;
+    int tiles = (H_unroll + TILE_WIDTH_SMALL - 1) / TILE_WIDTH_SMALL * TILE_WIDTH_SMALL;
 
-    int h0 = threadIdx.y;
-    int w0 = threadIdx.x;
-    int h_base = (blockIdx.z / tile_iw) * TILE_WIDTH;
-    int w_base = (blockIdx.z % tile_iw) * TILE_WIDTH;
-    int h = h_base + h0;
-    int w = w_base + w0;
+    int col = threadIdx.x + blockIdx.x * TILE_WIDTH_SMALL;
+    int row = threadIdx.y + blockIdx.y * TILE_WIDTH_SMALL;
 
-    // if (h < H_out && w < W_out) {
-    //     shared_x[threadIdx.y * TILE_WIDTH + threadIdx.x] = x4d(n, c, h, w);
-    // }
-    // __syncthreads();
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    int h_base = col / W_out;
+    int w_base = col % W_out;
 
     float tmp = 0;
 
-    // if (h < H_out && w < W_out) {
-    for (int c = 0; c < C; c++) {
-        for (int j = w; j < w_base + X_tile_width; j += TILE_WIDTH) {
-            for (int i = h; i < h_base + X_tile_width; i += TILE_WIDTH) {
-                shared_x[(i - h_base) * X_tile_width + j - w_base] =
-                    x4d(n, c, i, j);
-            }
+    for(int i = 0; i < tiles; i += TILE_WIDTH_SMALL) {
+        int col_index = (i + tx);
+
+        if(row < M && col_index < H_unroll) {
+          shared_k[ty][tx] = k[row * H_unroll + col_index];
+        } else {
+          shared_k[ty][tx] = 0;
         }
-        __syncthreads();
 
-        tmp += shared_x[(h0 + 0) * X_tile_width + w0 + 0] * k4d(m, c, 0, 0);
-        tmp += shared_x[(h0 + 0) * X_tile_width + w0 + 1] * k4d(m, c, 0, 1);
-        tmp += shared_x[(h0 + 0) * X_tile_width + w0 + 2] * k4d(m, c, 0, 2);
-        tmp += shared_x[(h0 + 0) * X_tile_width + w0 + 3] * k4d(m, c, 0, 3);
-        tmp += shared_x[(h0 + 0) * X_tile_width + w0 + 4] * k4d(m, c, 0, 4);
-        tmp += shared_x[(h0 + 0) * X_tile_width + w0 + 5] * k4d(m, c, 0, 5);
-        tmp += shared_x[(h0 + 0) * X_tile_width + w0 + 6] * k4d(m, c, 0, 6);
+        int row_index = (i + ty);
 
-        tmp += shared_x[(h0 + 1) * X_tile_width + w0 + 0] * k4d(m, c, 1, 0);
-        tmp += shared_x[(h0 + 1) * X_tile_width + w0 + 1] * k4d(m, c, 1, 1);
-        tmp += shared_x[(h0 + 1) * X_tile_width + w0 + 2] * k4d(m, c, 1, 2);
-        tmp += shared_x[(h0 + 1) * X_tile_width + w0 + 3] * k4d(m, c, 1, 3);
-        tmp += shared_x[(h0 + 1) * X_tile_width + w0 + 4] * k4d(m, c, 1, 4);
-        tmp += shared_x[(h0 + 1) * X_tile_width + w0 + 5] * k4d(m, c, 1, 5);
-        tmp += shared_x[(h0 + 1) * X_tile_width + w0 + 6] * k4d(m, c, 1, 6);
+        if(col < W_unroll && row_index < H_unroll) {
+            int x_index_c = row_index / K_squre;
+            int x_index_s = row_index % K_squre;
 
-        tmp += shared_x[(h0 + 2) * X_tile_width + w0 + 0] * k4d(m, c, 2, 0);
-        tmp += shared_x[(h0 + 2) * X_tile_width + w0 + 1] * k4d(m, c, 2, 1);
-        tmp += shared_x[(h0 + 2) * X_tile_width + w0 + 2] * k4d(m, c, 2, 2);
-        tmp += shared_x[(h0 + 2) * X_tile_width + w0 + 3] * k4d(m, c, 2, 3);
-        tmp += shared_x[(h0 + 2) * X_tile_width + w0 + 4] * k4d(m, c, 2, 4);
-        tmp += shared_x[(h0 + 2) * X_tile_width + w0 + 5] * k4d(m, c, 2, 5);
-        tmp += shared_x[(h0 + 2) * X_tile_width + w0 + 6] * k4d(m, c, 2, 6);
+            int x_index_row = x_index_s / K + h_base;
+            int x_index_col = x_index_s % K + w_base;
 
-        tmp += shared_x[(h0 + 3) * X_tile_width + w0 + 0] * k4d(m, c, 3, 0);
-        tmp += shared_x[(h0 + 3) * X_tile_width + w0 + 1] * k4d(m, c, 3, 1);
-        tmp += shared_x[(h0 + 3) * X_tile_width + w0 + 2] * k4d(m, c, 3, 2);
-        tmp += shared_x[(h0 + 3) * X_tile_width + w0 + 3] * k4d(m, c, 3, 3);
-        tmp += shared_x[(h0 + 3) * X_tile_width + w0 + 4] * k4d(m, c, 3, 4);
-        tmp += shared_x[(h0 + 3) * X_tile_width + w0 + 5] * k4d(m, c, 3, 5);
-        tmp += shared_x[(h0 + 3) * X_tile_width + w0 + 6] * k4d(m, c, 3, 6);
+            shared_x[ty][tx] = x4d(x_index_c, x_index_row, x_index_col);
+        } else {
+            shared_x[ty][tx] = 0;
+        
+        }
 
-        tmp += shared_x[(h0 + 4) * X_tile_width + w0 + 0] * k4d(m, c, 4, 0);
-        tmp += shared_x[(h0 + 4) * X_tile_width + w0 + 1] * k4d(m, c, 4, 1);
-        tmp += shared_x[(h0 + 4) * X_tile_width + w0 + 2] * k4d(m, c, 4, 2);
-        tmp += shared_x[(h0 + 4) * X_tile_width + w0 + 3] * k4d(m, c, 4, 3);
-        tmp += shared_x[(h0 + 4) * X_tile_width + w0 + 4] * k4d(m, c, 4, 4);
-        tmp += shared_x[(h0 + 4) * X_tile_width + w0 + 5] * k4d(m, c, 4, 5);
-        tmp += shared_x[(h0 + 4) * X_tile_width + w0 + 6] * k4d(m, c, 4, 6);
+    __syncthreads();
 
-        tmp += shared_x[(h0 + 5) * X_tile_width + w0 + 0] * k4d(m, c, 5, 0);
-        tmp += shared_x[(h0 + 5) * X_tile_width + w0 + 1] * k4d(m, c, 5, 1);
-        tmp += shared_x[(h0 + 5) * X_tile_width + w0 + 2] * k4d(m, c, 5, 2);
-        tmp += shared_x[(h0 + 5) * X_tile_width + w0 + 3] * k4d(m, c, 5, 3);
-        tmp += shared_x[(h0 + 5) * X_tile_width + w0 + 4] * k4d(m, c, 5, 4);
-        tmp += shared_x[(h0 + 5) * X_tile_width + w0 + 5] * k4d(m, c, 5, 5);
-        tmp += shared_x[(h0 + 5) * X_tile_width + w0 + 6] * k4d(m, c, 5, 6);
-
-        tmp += shared_x[(h0 + 6) * X_tile_width + w0 + 0] * k4d(m, c, 6, 0);
-        tmp += shared_x[(h0 + 6) * X_tile_width + w0 + 1] * k4d(m, c, 6, 1);
-        tmp += shared_x[(h0 + 6) * X_tile_width + w0 + 2] * k4d(m, c, 6, 2);
-        tmp += shared_x[(h0 + 6) * X_tile_width + w0 + 3] * k4d(m, c, 6, 3);
-        tmp += shared_x[(h0 + 6) * X_tile_width + w0 + 4] * k4d(m, c, 6, 4);
-        tmp += shared_x[(h0 + 6) * X_tile_width + w0 + 5] * k4d(m, c, 6, 5);
-        tmp += shared_x[(h0 + 6) * X_tile_width + w0 + 6] * k4d(m, c, 6, 6);
-
-        __syncthreads();
+    #pragma unroll
+    for(int j = 0; j < TILE_WIDTH_SMALL; j++) {
+      tmp += shared_x[j][tx] * shared_k[ty][j];
     }
-    if (n < B && m < M && h < H_out && w < W_out) {
-        // y4d(b, m, h, w) = acc;
-        y4d(n, m, h, w) = tmp;
+
+    __syncthreads();
+
+  }
+
+  if(row < M && col < W_unroll) {
+    y4d(blockIdx.z, row, col) = tmp;
+  }
+
+}
+
+__global__ void forward_kernel_large(float* __restrict__ y, const float* __restrict__ x, const float* __restrict__ k,
+                            int M, int C, int H, int W, int H_unroll, int W_unroll) {
+    /*
+    Modify this function to implement the forward pass described in
+    Chapter 16. We have added an additional dimension to the tensors to
+    support an entire mini-batch The goal here is to be correct AND fast. We
+    have some nice #defs for you below to simplify indexing. Feel free to
+    use them, or create your own.
+    */
+    __shared__ float shared_k[TILE_WIDTH_LARGE][TILE_WIDTH_LARGE];
+    __shared__ float shared_x[TILE_WIDTH_LARGE][TILE_WIDTH_LARGE];
+
+    int b = blockIdx.z * C * H * W;
+
+    const int H_out = H - K_minus_one;
+    const int W_out = W - K_minus_one;
+
+    int tiles = (H_unroll + TILE_WIDTH_LARGE - 1) / TILE_WIDTH_LARGE * TILE_WIDTH_LARGE;
+
+    int col = threadIdx.x + blockIdx.x * TILE_WIDTH_LARGE;
+    int row = threadIdx.y + blockIdx.y * TILE_WIDTH_LARGE;
+
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    int h_base = col / W_out;
+    int w_base = col % W_out;
+
+    float tmp = 0;
+
+    for(int i = 0; i < tiles; i += TILE_WIDTH_LARGE) {
+        int col_index = (i + tx);
+
+        if(row < M && col_index < H_unroll) {
+          shared_k[ty][tx] = k[row * H_unroll + col_index];
+        } else {
+          shared_k[ty][tx] = 0;
+        }
+
+        int row_index = (i + ty);
+
+        if(col < W_unroll && row_index < H_unroll) {
+            int x_index_c = row_index / K_squre;
+            int x_index_s = row_index % K_squre;
+
+            int x_index_row = x_index_s / K + h_base;
+            int x_index_col = x_index_s % K + w_base;
+
+            shared_x[ty][tx] = x4d(x_index_c, x_index_row, x_index_col);
+        } else {
+            shared_x[ty][tx] = 0;
+        
+        }
+
+    __syncthreads();
+
+    #pragma unroll
+    for(int j = 0; j < TILE_WIDTH_LARGE; j++) {
+      tmp += shared_x[j][tx] * shared_k[ty][j];
     }
+
+    __syncthreads();
+  }
+
+  if(row < M && col < W_unroll) {
+    y4d(blockIdx.z, row, col) = tmp;
+  }
+
 }
 
 /*
@@ -154,26 +178,20 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y,
     const int C = x.shape_[1];
     const int H = x.shape_[2];
     const int W = x.shape_[3];
-    const int K = w.shape_[3];
 
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    const int tile_iw = ceil(W_out / (1.0 * TILE_WIDTH));
-    const int tile_ih = ceil(H_out / (1.0 * TILE_WIDTH));
-    const int Z = tile_iw * tile_ih;
+    int W_unroll = (H - K_minus_one) * (W - K_minus_one);
+    int H_unroll = C * K_squre;
 
-    // Set the kernel dimensions
-    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
-    dim3 gridDim(B, M, Z);
+    if(C == 12) {
+        dim3 gridDim((W_unroll + TILE_WIDTH_LARGE - 1) / TILE_WIDTH_LARGE, (M + TILE_WIDTH_LARGE - 1) / TILE_WIDTH_LARGE, B);
+        dim3 blockDim(TILE_WIDTH_LARGE, TILE_WIDTH_LARGE, 1);
+        forward_kernel_large<<<gridDim, blockDim>>>(y.dptr_, x.dptr_, w.dptr_, M, C, H, W, H_unroll, W_unroll);
+    } else {
+        dim3 gridDim((W_unroll + TILE_WIDTH_SMALL - 1) / TILE_WIDTH_SMALL, (M + TILE_WIDTH_SMALL - 1) / TILE_WIDTH_SMALL, B);
+        dim3 blockDim(TILE_WIDTH_SMALL, TILE_WIDTH_SMALL, 1);
 
-    size_t size_of_share =
-        sizeof(float) * ((TILE_WIDTH + K - 1) * (TILE_WIDTH + K - 1));
-
-    cudaMemcpyToSymbol(k_shared, w.dptr_, M * C * K * K * sizeof(float));
-
-    // Call the kernel
-    forward_kernel<<<gridDim, blockDim, size_of_share>>>(
-        y.dptr_, x.dptr_, w.dptr_, B, M, C, H, W, K);
+        forward_kernel_small<<<gridDim, blockDim>>>(y.dptr_, x.dptr_, w.dptr_, M, C, H, W, H_unroll, W_unroll);
+    }   
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
